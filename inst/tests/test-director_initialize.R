1cfc132b02be11b0c0a00de8aee749bad25e0e8e
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-07-18 13:25:29 -0500

    tests for director$initialize method

---
+++ inst/tests/test-director_initialize.R
@@ -0,0 +1,12 @@
+context('director_initialize')
+
+test_that('it errors when a directory does not exist', {
+  expect_error(director('-non-existent-'), 'directory does not exist')
+})
+
+test_that('it errors when given a filename rather than a directory', {
+  within_file_structure(list('somefile'),
+    expect_error(director(file.path(tempdir, 'somefile')), 'that is a file')                        
+  )
+})
+
