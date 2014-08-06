f7a0ef39af361e7f7b33b124209fa076bc0e9f7e
RobertZK <technoguyrob@gmail.com>
2014-08-06 11:08:34 -0500

    add level two dependencies

--- inst/tests/test-parsers.R
+++ inst/tests/test-parsers.R
@@ -28,3 +28,21 @@ test_that("it remembers dependencies", {
   })
 })
 
+test_that("it remembers depth-2 dependencies", {
+  within_file_structure(list(blah = list('one.R'), foo = list('two.R'),
+                             faz = list('three.R')), {
+    d <- director(tempdir)
+    d$register_parser('blah', function() { director$resource('foo/two')$value() })
+    d$register_parser('foo', function() { director$resource('faz/three')$value() })
+    d$register_parser('faz', function() { "test" })
+    r <- d$resource('blah/one'); r$value()
+    r <- d$resource('blah/one'); r$value()
+    expect_false(r$modified)
+    Sys.sleep(1)
+    touch(file.path(tempdir, 'foo', 'two.R'))
+    r <- d$resource('blah/one') # cache the resource info
+    r$value()
+    expect_true(r$modified)
+  })
+})
+
