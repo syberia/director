2a6fd95118d1a1ffa1363e14a22f42ee1b177402
RobertZK <technoguyrob@gmail.com>
2014-08-06 18:29:53 -0500

    use overwrite instead of soft

--- R/parsers.R
+++ R/parsers.R
@@ -2,8 +2,8 @@
 #'
 #' @param path character. The prefix to look for in the director.
 #' @param parser directorParser. 
-#' @param soft logical. If \code{TRUE}, \code{register_parser} will faily
-#'   silently instead of erroring if the path already has a registered
+#' @param overwrrite logical. If \code{TRUE}, \code{register_parser} will overwrite
+#'   the route instead of erroring if the path already has a registered
 #'   parser. The default is \code{FALSE}.
 #' @examples
 #' \dontrun{
@@ -12,13 +12,12 @@
 #'   r <- d$resource('models/some_model.R')
 #'   r$value() # Will print: I am a models/some_model, a model!
 #' }
-register_parser <- function(path, parser, soft = FALSE) {
+register_parser <- function(path, parser, overwrite = FALSE) {
   stopifnot(is.character(path))
   stopifnot(is.function(parser))
   stopifnot(length(formals(parser)) == 0) # Use environment to provide info
   
-  if (is.element(path, names(.parsers))) {
-    if (isTRUE(soft)) return()
+  if (is.element(path, names(.parsers)) && !isTRUE(overwrite)) {
     stop("Parser already registered for path ", sQuote(path))
   }
 
