aff20a4dabb064616cb330ae17f13bf48449de0c
RobertZK <technoguyrob@gmail.com>
2014-08-18 23:53:47 -0500

    test that director$find correctly finds a simple file in the root

--- inst/tests/test-director_find.R
+++ inst/tests/test-director_find.R
@@ -2,10 +2,13 @@ context('director$find method')
 require(testthatsomemore)
 
 test_that('it correctly finds no files in an empty directory', {
-  within_file_structure(list(), {
-    d <- director(tempdir)
+  within_file_structure(list(), { d <- director(tempdir)
     expect_equal(0, length(d$find('')), info = 'No files should have been found.')
   })
 })
 
-
+test_that('it correctly finds a simple file in the root', {
+  within_file_structure(list('hello.R'), { d <- director(tempdir)
+    expect_identical('/hello', d$find('hello'), info = 'The find method should have found the file "hello".')
+  })
+})
