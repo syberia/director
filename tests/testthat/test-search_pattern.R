a058db21716cf651e396a81d437c0972f01333fd
RobertZK <technoguyrob@gmail.com>
2015-03-01 21:30:19 -0600

    typecheck pattern method in search_pattern

--- tests/testthat/test-search_pattern.R
+++ tests/testthat/test-search_pattern.R
@@ -1,4 +1,5 @@
 context("search_pattern")
+library(testthatsomemore)
 
 test_that("it errors if an invalid method is provided", {
   invalid_methods <- list(NULL, 5, "bloo", "partia", a ~ b)
@@ -7,3 +8,16 @@ test_that("it errors if an invalid method is provided", {
   })
 })
 
+test_that("it does not error on valid methods", {
+  valid_methods <- c("wildcard", "partial", "exact", "WILDcard", "parTIAL", "eXact")
+  lapply(valid_methods, function(method) {
+    assert(search_pattern("", method))
+  })
+})
+
+test_that("it errors if a non-character pattern is provided", {
+  invalid_patterns <- list(NULL, 5, a ~ b, list())
+  lapply(invalid_patterns, function(pattern) {
+    expect_error(search_pattern(pattern, "wildcard"), "pattern must be")
+  })
+})
