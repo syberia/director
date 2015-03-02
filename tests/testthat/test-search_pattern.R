41df11f9b378b880f136c7dc0e30c8f465bd3077
RobertZK <technoguyrob@gmail.com>
2015-03-01 22:34:54 -0600

    fix tests for error checking on search_pattern

--- tests/testthat/test-search_pattern.R
+++ tests/testthat/test-search_pattern.R
@@ -5,7 +5,7 @@ describe("search_pattern S3 class", {
   test_that("it errors if an invalid method is provided", {
     invalid_methods <- list(NULL, 5, "bloo", "partia", a ~ b)
     lapply(invalid_methods, function(method) {
-      expect_error(search_pattern("", method), "must be")
+      expect_error(search_pattern("", method), "must be|Invalid search")
     })
   })
 
