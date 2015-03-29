af31a995ff4e81603f6aa4908f9ae2ff150276e8
RobertZK <technoguyrob@gmail.com>
2015-03-28 19:38:53 -0500

    Don't actually load the memoise package

--- R/utils.r
+++ R/utils.r
@@ -13,11 +13,6 @@ accessor_method <- function(attr) {
 
 #' Attempt to memoize a function using the memoise package.
 #' 
-#' This function will load the \code{memoise} package if it is
-#' available, or do nothing otherwise.
-#'
-#' (Blame Hadley for the spelling of memoise.)
-#'
 #' @param fn function. The function to memoize.
 #' @return nothing, but \code{try_memoize} will use non-standard
 #'   evaluation to memoize in the calling environment.
