44cd5905c5c9473a6d6a994f4b8c6aa58ab44b05
RobertZK <technoguyrob@gmail.com>
2015-03-29 17:06:16 -0500

    Remove check files

--- director.Rcheck/00_pkg_src/director/tests/testthat/test-utils.R
+++
@@ -1,8 +0,0 @@
-context("utils")
-
-describe("enforce_type", {
-  test_that("it errors on a simple example as expected", {
-    x <- 1
-    expect_error(enforce_type(x, "logical", "myfunction"), "parameter must be a")
-  })
-})
--- director.Rcheck/tests/test-all.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(testthatsomemore)
-library("director")
-test_check("director")
