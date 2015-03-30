5fbe5d711071e64f922fac76a21f091b08245d12
RobertZK <technoguyrob@gmail.com>
2015-03-30 10:48:00 -0500

    test error handling of test-utils<test-utils.R operator

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,11 @@ describe("enforce_type", {
     expect_error(enforce_type(x, "logical", "myfunction"), "parameter must be a")
   })
 })
+
+describe("%<<% operator", {
+  test_that("it errors unless objects are named", {
+    expect_error(list(1) %<<% list(2))
+    expect_error(list(a = 1) %<<% list(2))
+    expect_error(list(1) %<<% list(a = 2))
+  })
+})
