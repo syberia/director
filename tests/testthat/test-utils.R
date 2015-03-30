47cfecbcb71c9959e336529afc789cea210e85a9
RobertZK <technoguyrob@gmail.com>
2015-03-30 10:53:26 -0500

    test inserting into environments using test-utils<test-utils.R

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -14,15 +14,32 @@ describe("%<<% operator", {
     expect_error(list(1) %<<% list(a = 2))
   })
 
-  test_that("it can insert list elements", {
-    x <- list(a = 1)
-    expect_identical(x %<<% list(b = 2), list(a = 1, b = 2))
-    expect_identical(x %<<% list(a = 2), list(a = 2))
+  describe("inserting into lists", {
+    test_that("it can insert list elements", {
+      x <- list(a = 1)
+      expect_identical(x %<<% list(b = 2), list(a = 1, b = 2))
+      expect_identical(x %<<% list(a = 2), list(a = 2))
+    })
+
+    test_that("it can insert environments into lists", {
+      x <- list(a = 1)
+      expect_identical(x %<<% list2env(list(b = 2)), list(a = 1, b = 2))
+      expect_identical(x %<<% list2env(list(a = 2)), list(a = 2))
+    })
   })
 
-  test_that("it can insert environments into lists", {
-    x <- list(a = 1)
-    expect_identical(x %<<% list2env(list(b = 2)), list(a = 1, b = 2))
-    expect_identical(x %<<% list2env(list(a = 2)), list(a = 2))
+  describe("inserting into environments", {
+    tolist <- function(x) {
+      x <- as.list(x)
+      x[sort(names(x))]
+    }
+
+    test_that("it can insert lists into environments", {
+      x <- list2env(list(a = 1))
+      expect_identical(tolist(x %<<% list(b = 2)), list(a = 1, b = 2))
+      x <- list2env(list(a = 1))
+      expect_identical(tolist(x %<<% list(a = 2)), list(a = 2))
+    })
+
   })
 })
