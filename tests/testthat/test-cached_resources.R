10e06a29f18abcda9071baab098e41299141954e
RobertZK <technoguyrob@gmail.com>
2015-03-30 18:15:42 -0500

    fully working v3 tests

--- tests/testthat/test-cached_resources.R
+++ tests/testthat/test-cached_resources.R
@@ -4,16 +4,16 @@ library(testthatsomemore)
 test_that('it can cache a NULL resource correctly', {
   within_file_structure(list(foo.R = 'cat("only once")'), { d <- director(tempdir)  
     d$register_parser('foo', function() { output }, cache = TRUE)                      
-    expect_output(invisible(replicate(5, d$resource('foo')$value())), '^only once$')
-    expect_null(d$resource('foo')$value())
+    expect_output(invisible(replicate(5, d$resource('foo'))), '^only once$')
+    expect_null(d$resource('foo'))
   })
 })
 
 test_that('it can cache a resource correctly', {
   within_file_structure(list(foo.R = 'cat("only once"); list(x = 1)'), { d <- director(tempdir)
     d$register_parser('foo', function() { output }, cache = TRUE)
-    expect_output(invisible(replicate(5, d$resource('foo')$value())), '^only once$')
-    expect_identical(d$resource('foo')$value(), list(x = 1))
+    expect_output(invisible(replicate(5, d$resource('foo'))), '^only once$')
+    expect_identical(d$resource('foo'), list(x = 1))
   })
 })
 
