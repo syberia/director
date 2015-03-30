d9b1e39203c9851234e8a6c23ebd5344b6e9033f
RobertZK <technoguyrob@gmail.com>
2015-03-30 10:09:15 -0500

    explain the dream

--- R/resource_tower.R
+++ R/resource_tower.R
@@ -1,5 +1,13 @@
 
 # Construct a resource-compiling tower.
 resource_tower <- function(director, name) {
+  # This is the dream! Now we have to make it happen.
 
+  # virtual_check        %>>%
+  # modification_tracker %>>%
+  # dependency_tracker   %>>% 
+  # caching_layer        %>>%
+  # preprocessor         %>>%
+  # parser               %>>%
+  # as.active_resource(resource)
 }
