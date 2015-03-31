dfebb5758792799ca011c14908b8bfe12752d66e
RobertZK <technoguyrob@gmail.com>
2015-03-31 08:35:41 -0500

    check that  overwrite works correctly on director R6 class

--- tests/testthat/test-director_initialize.R
+++ tests/testthat/test-director_initialize.R
@@ -1,13 +1,23 @@
-context('director$initialize')
+context("director$initialize")
 require(testthatsomemore)
 
-test_that('it errors when a directory does not exist', {
-  expect_error(director('-non-existent-'), 'directory does not exist')
+test_that("it errors when a directory does not exist", {
+  expect_error(director("-non-existent-"), "directory does not exist")
 })
 
-test_that('it errors when given a filename rather than a directory', {
-  within_file_structure(list('somefile'),
-    expect_error(director(file.path(tempdir, 'somefile')), 'that is a file')                        
+test_that("it errors when given a filename rather than a directory", {
+  within_file_structure(list("somefile"),
+    expect_error(director(file.path(tempdir, "somefile")), "that is a file")
   )
 })
 
+test_that("it is not possible to call any other method on the director class than $new", {
+  expect_error(director$foo())
+  expect_error(director$classname)
+  expect_error(director$active)
+  expect_error(director$inherit)
+  expect_error(director$private)
+  expect_error(director$public)
+  expect_error(director$parent_env)
+  expect_error(director$lock)
+})
