4042aeb70b4edaf4fc984318fc20c0c55d2729be
RobertZK <technoguyrob@gmail.com>
2015-03-31 08:48:46 -0500

    check that directors with non-scalar project names error

--- tests/testthat/test-director_initialize.R
+++ tests/testthat/test-director_initialize.R
@@ -1,25 +1,32 @@
 context("director$initialize")
 require(testthatsomemore)
 
-test_that("it errors when a directory does not exist", {
-  expect_error(director("-non-existent-"), "directory does not exist")
-})
+describe("invalid inputs", {
+  test_that("it errors when a directory does not exist", {
+    expect_error(director("-non-existent-"), "directory does not exist")
+  })
 
-test_that("it errors when given a filename rather than a directory", {
-  within_file_structure(list("somefile"),
-    expect_error(director(file.path(tempdir, "somefile")), "that is a file")
-  )
-})
+  test_that("it errors when given a filename rather than a directory", {
+    within_file_structure(list("somefile"),
+      expect_error(director(file.path(tempdir, "somefile")), "that is a file")
+    )
+  })
 
-test_that("it is not possible to call any other method on the director class than $new", {
-  expect_error(director$foo())
-  expect_error(director$classname)
-  expect_error(director$active)
-  expect_error(director$inherit)
-  expect_error(director$private)
-  expect_error(director$public)
-  expect_error(director$parent_env)
-  expect_error(director$lock)
+  test_that("it is not possible to call any other method on the director class than $new", {
+    expect_error(director$foo())
+    expect_error(director$classname)
+    expect_error(director$active)
+    expect_error(director$inherit)
+    expect_error(director$private)
+    expect_error(director$public)
+    expect_error(director$parent_env)
+    expect_error(director$lock)
+  })
+
+  test_that("it errors if project name is not scalar", {
+    expect_error(director$new(tempdir(), character(0)), "must be a scalar")
+    expect_error(director$new(tempdir(), c("foo", "bar")), "must be a scalar")
+  })
 })
 
 test_that("is.director works", {
