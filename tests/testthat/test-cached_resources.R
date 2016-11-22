eb006eed9832454c342d102d235e98f057a13826
RobertZK <technoguyrob@gmail.com>
2016-11-22 13:05:50 -0600

    Explicitly capture.output.

--- tests/testthat/test-cached_resources.R
+++ tests/testthat/test-cached_resources.R
@@ -9,7 +9,7 @@ test_that("pinging a cached resource's dependencies wipes the cache", {
     d$register_parser('foo', function() director$resource('baz'))
     d$register_parser('baz', function() director$resource('bum'))
     expect_output(d$resource('bar'), "^bar(NULL)?$", info = "expected bar")
-    expect_output(d$resource('bar'), "^(NULL)?$", info = "expected nothing")
+    expect_true(grepl("^(NULL)?$", capture.output(d$resource('bar'))))
     writeLines("cat('foo')", file.path(tempdir, "bum.R"))
     touch_file(file.path(tempdir, "bum.R"))
     expect_output(d$resource('bar'), "^barfoo", info = "expected barfoo")
